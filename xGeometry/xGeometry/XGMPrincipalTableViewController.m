7ed5b14eedb83985635c322145993550b2da492f
juliothefox <juliothefox@r7.com>
2014-08-08 17:57:40 -0300

    Metade da programação da navegação concluída
    Navegação das áreas concluída, falta dos volumes.

--- xGeometry/xGeometry/XGMPrincipalTableViewController.m
+++ xGeometry/xGeometry/XGMPrincipalTableViewController.m
@@ -7,7 +7,13 @@
 //
 
 #import "XGMPrincipalTableViewController.h"
-#define ISO 3
+#import "XGMTrianguloViewController.h"
+
+#define AREA_CILINDRO indexPath.section == 0 && indexPath.row == 0
+#define AREA_CIRCULO indexPath.section == 0 && indexPath.row == 1
+#define AREA_CONE indexPath.section == 0 && indexPath.row == 2
+#define AREA_TRIANGULO indexPath.section == 0 && indexPath.row == 3
+#define AREA_QUADRADO indexPath.section == 0 && indexPath.row == 4
 
 @interface XGMPrincipalTableViewController ()
 
@@ -34,7 +40,7 @@
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 
-    self.arrayPlist = [NSArray arrayWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"database" ofType:@"plist"]];
+    self.arrayPlist = [NSArray arrayWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"principalTableView" ofType:@"plist"]];
     
 }
 
@@ -83,6 +89,37 @@
     return cell;
 }
 
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if(AREA_CILINDRO){
+        [self performSegueWithIdentifier:@"areaCilindro" sender:self];
+        return;
+    }
+    if(AREA_CIRCULO){
+        [self performSegueWithIdentifier:@"areaCirculo" sender:self];
+        return;
+    }
+    if(AREA_CONE){
+        [self performSegueWithIdentifier:@"areaCone" sender:self];
+        return;
+    }
+    if(AREA_TRIANGULO){
+        [self performSegueWithIdentifier:@"areaTriangulo" sender:self];
+        return;
+    }
+    if(AREA_QUADRADO){
+        [self performSegueWithIdentifier:@"areaQuadrado" sender:self];
+        return;
+    }
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Prepara para transitar para outra viewController.
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
 /*
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
@@ -118,17 +155,6 @@
 {
     // Return NO if you do not want the item to be re-orderable.
     return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
 }
 */
 
---
+++ xGeometry/xGeometry/XGMTrianguloViewController.h
@@ -0,0 +1,13 @@
+//
+//  XGMTrianguloViewController.h
+//  xGeometry
+//
+//  Created by Júlio Menezes Noronha on 08/08/14.
+//  Copyright (c) 2014 Júlio César Menezes Noronha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XGMTrianguloViewController : UIViewController
+
+@end
