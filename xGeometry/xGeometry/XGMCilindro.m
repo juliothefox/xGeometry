354e164b1e6d1271d486fca593bbd2cc8419fe46
juliothefox <juliothefox@r7.com>
2014-08-28 15:28:10 -0300

    Cálculo do cilindro corrigido
    Corrigido erro no cálculo da área total do cilindro

--- xGeometry/xGeometry/XGMCilindro.m
+++ xGeometry/xGeometry/XGMCilindro.m
@@ -18,9 +18,9 @@
     NSMutableString *str = [[NSMutableString alloc]init];
     
     [str appendString:[NSString stringWithFormat:@"Considerando π como 3,14:\nAl = 2 . π . r . h\n"]];
-    [str appendString:[NSString stringWithFormat:@"Al = 2 . π . %d . %d\n",self.base.raio,self.altura]];
-    [str appendString:[NSString stringWithFormat:@"Al = %d . π\n",2 *self.base.raio * self.altura]];
-    [str appendString:[NSString stringWithFormat:@"Al = %f u²",2 *self.base.raio * self.altura * 3.14]];
+    [str appendString:[NSString stringWithFormat:@"Al = 2 . π . %d . %d\n",(int)_base.raio,(int)_altura]];
+    [str appendString:[NSString stringWithFormat:@"Al = %d . π\n",2 *(int)_base.raio * (int)_altura]];
+    [str appendString:[NSString stringWithFormat:@"Al = %f u²",2 *(int)_base.raio * (int)_altura * 3.14]];
     
     return str;
 }
@@ -32,19 +32,25 @@
     
     NSMutableString *str = [[NSMutableString alloc]init];
     
-    [str appendString:[NSString stringWithFormat:@"Considerando π como 3,14:\nAl = 2 . π . r . h\n"]];
-    [str appendString:[NSString stringWithFormat:@"Al = 2 . π . %d . %d\n",self.base.raio,self.altura]];
-    [str appendString:[NSString stringWithFormat:@"Al = %d .π\n",2 *self.base.raio * self.altura]];
-    [str appendString:[NSString stringWithFormat:@"Al = %f u²\n\n",2 *self.base.raio * self.altura * 3.14]];
+//    [str appendString:[NSString stringWithFormat:@"Considerando π como 3,14:\nAl = 2 . π . r . h\n"]];
+//    [str appendString:[NSString stringWithFormat:@"Al = 2 . π . %d . %d\n",(int)_base.raio,(int)_altura]];
+//    [str appendString:[NSString stringWithFormat:@"Al = %d .π\n",2 *(int)_base.raio * (int)_altura]];
+//    [str appendString:[NSString stringWithFormat:@"Al = %f u²\n\n",2 *(int)_base.raio * (int)_altura * 3.14]];
+//    
+//    [str appendString:[NSString stringWithFormat:@"Ab = π .r²\n"]];
+//    [str appendString:[NSString stringWithFormat:@"Ab = π .%d²\n",(int)_base.raio]];
+//    [str appendString:[NSString stringWithFormat:@"Ab = π .%d\n",(int)_base.raio * (int)_base.raio]];
+//    [str appendString:[NSString stringWithFormat:@"Ab = %f u²\n\n",(int)_base.raio * (int)_base.raio * 3.14]];
+//    
+//    [str appendString:[NSString stringWithFormat:@"At = Al + 2 . Ab\n"]];
+//    [str appendString:[NSString stringWithFormat:@"At = %f + 2 . %f\n",2 *(int)_base.raio * (int)_altura * 3.14,(int)_base.raio * (int)_base.raio * 3.14]];
+//    [str appendString:[NSString stringWithFormat:@"At = %f u²\n",2 *(int)_base.raio * (int)_altura * 3.14 + (int)_base.raio * (int)_base.raio * 3.14]];
     
-    [str appendString:[NSString stringWithFormat:@"Ab = π .r²\n"]];
-    [str appendString:[NSString stringWithFormat:@"Ab = π .%d²\n",self.base.raio]];
-    [str appendString:[NSString stringWithFormat:@"Ab = π .%d\n",self.base.raio * self.base.raio]];
-    [str appendString:[NSString stringWithFormat:@"Ab = %f u²\n\n",self.base.raio * self.base.raio * 3.14]];
-    
-    [str appendString:[NSString stringWithFormat:@"At = Al + 2 . Ab\n"]];
-    [str appendString:[NSString stringWithFormat:@"At = %f + 2 . %f\n",2 *self.base.raio * self.altura * 3.14,self.base.raio * self.base.raio * 3.14]];
-    [str appendString:[NSString stringWithFormat:@"At = %f u²\n",2 *self.base.raio * self.altura * 3.14 + self.base.raio * self.base.raio * 3.14]];
+    [str appendString:[NSString stringWithFormat:@"Considerando π como 3,14:\nAt = 2 . π . r (r + h)\n"]];
+    [str appendString:[NSString stringWithFormat:@"At = 2 . π . %d (%d + %d)\n",(int)_base.raio,(int)_base.raio,(int)_altura]];
+    [str appendString:[NSString stringWithFormat:@"At = %d . π (%d)\n",(int) (2 * _base.raio), (int)(_base.raio + _altura)]];
+    [str appendString:[NSString stringWithFormat:@"At = %d . π\n",(int) (2 * _base.raio * (_base.raio + _altura))]];
+    [str appendString:[NSString stringWithFormat:@"At = %f\n",6.28 * _base.raio * (_base.raio + _altura)]];
     
     return str;
 }
@@ -57,9 +63,9 @@
     NSMutableString *str = [[NSMutableString alloc]init];
     
     [str appendString:[NSString stringWithFormat:@"Considerando π como 3,14:\nVc = π . r² . h\n"]];
-    [str appendString:[NSString stringWithFormat:@"Vc = π . %d² . %d\n",self.base.raio,self.altura]];
-    [str appendString:[NSString stringWithFormat:@"Vc = %d . π\n",self.base.raio * self.base.raio * self.altura]];
-    [str appendString:[NSString stringWithFormat:@"Vc = %f u³\n",self.base.raio * self.base.raio * self.altura * 3.14]];
+    [str appendString:[NSString stringWithFormat:@"Vc = π . %d² . %d\n",(int)_base.raio,(int)_altura]];
+    [str appendString:[NSString stringWithFormat:@"Vc = %d . π\n",(int)_base.raio * (int)_base.raio * (int)_altura]];
+    [str appendString:[NSString stringWithFormat:@"Vc = %f u³\n",(int)_base.raio * (int)_base.raio * (int)_altura * 3.14]];
     
     return str;
 }
